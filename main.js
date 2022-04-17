e27e89b14dc08319a41236a954222ac28211b59c
siddhantsanghvi2009 <87454355+siddhantsanghvi2009@users.noreply.github.com>
2022-04-17 19:59:16 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+console.log("My First Console Project")
+var first_name = "Siddhant";
+var last_name = " Sanghvi";
+var a = 10;
+var b = 5;
+
+var fullname = first_name.concat(last_name);
+console.log(fullname);
+
+var add = a + b ;
+console.log(add);
+
+var subtract = a - b ; 
+console.log(subtract);
+
+var multiply = a * b ; 
+console.log (multiply);
+
+var divide = a / b ;
+console.log(divide);
